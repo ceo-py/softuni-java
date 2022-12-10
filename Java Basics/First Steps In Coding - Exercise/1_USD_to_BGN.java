b625339763737cc53b98f0f0b048f662abcf0c71
ceo-py <sfmf@abv.bg>
2022-12-10 12:09:21 +0200

    initial solution

---
+++ Java Basics/First Steps In Coding - Exercise/1_USD_to_BGN.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // Read the user's information
+    Scanner sc = new Scanner(System.in);
+    float usd = sc.nextFloat();
+
+    // Calculate the result and print it
+    System.out.println(usd * 1.79549);
+  }
+}
