a20271a9696cdec95a2c252ad6edb183a34eb430
ceo-py <sfmf@abv.bg>
2022-12-10 11:53:19 +0200

    initial solution

---
+++ Java Basics/ First Steps In Coding - Lab/4_inches_to_centimeters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    float a = sc.nextFloat();
+
+    System.out.println(a * 2.54);
+  }
+}
