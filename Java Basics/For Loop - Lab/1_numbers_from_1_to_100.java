3c049bb78007d6bc5c1f427d7d0aa5cc5e41a0a4
ceo-py <sfmf@abv.bg>
2022-12-12 15:50:25 +0200

    initial solution

---
+++ Java Basics/For Loop - Lab/1_numbers_from_1_to_100.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
