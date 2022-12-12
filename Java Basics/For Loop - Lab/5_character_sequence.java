272814c7dcf2a671e954f44d92fd4e0b44889053
ceo-py <sfmf@abv.bg>
2022-12-12 15:56:36 +0200

    initial solution

---
+++ Java Basics/For Loop - Lab/5_character_sequence.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            System.out.println(symbol);
+        }
+    }
+}
