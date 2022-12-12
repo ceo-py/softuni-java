243c92ff0e3de3de2b017bfa090595aa0fa051b9
ceo-py <sfmf@abv.bg>
2022-12-12 16:06:51 +0200

    initial solution

---
+++ Java Basics/For Loop - Lab/8_number_sequence.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numbersInput = Integer.parseInt(scanner.nextLine());
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= numbersInput; i++) {
+            int numbers = Integer.parseInt(scanner.nextLine());
+            if (numbers > maxNum) {
+                maxNum = numbers;
+            }
+            if (numbers < minNum) {
+                minNum = numbers;
+            }
+        }
+        System.out.printf("Max number: %d", maxNum);
+        System.out.println();
+        System.out.printf("Min number: %d", minNum);
+
+    }
+}
