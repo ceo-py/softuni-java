d9193fa70b15b7021d66d48b831acaf9d3baee8e
ceo-py <sfmf@abv.bg>
2022-12-14 13:43:59 +0200

    initial solution

---
+++ Java Basics/Nested Loops - More Exercises/3_lucky_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lucky_Numbers_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int a = 1; a <= 9; a++) {
+            for (int b = 1; b <= 9; b++) {
+                for (int c = 1; c <= 9; c++) {
+                    for (int d = 1; d <= 9; d ++) {
+                        if ((a + b) == (c + d)) {
+                            if (n % (a + b) == 0) {
+                                System.out.printf("%d%d%d%d ", a, b, c, d );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
