08faf9c986f657bfef47cf265a64726307444185
ceo-py <sfmf@abv.bg>
2022-12-14 11:25:10 +0200

    initial solution

--- Java Basics/Conditional Statements Advanced - Exercise/7_hotel_room.java
+++ Java Basics/Conditional Statements Advanced - Exercise/7_hotel_room.java
@@ -1,40 +1,63 @@
-package com.company;
-
 import java.util.Scanner;
 
-public class Main {
-
+public class HotelRoom07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String month = scanner.nextLine();
-        int numberOfNights = Integer.parseInt(scanner.nextLine());
 
-        double pricePerNightForStudio = 0.0;
-        double pricePerNightForApartment = 0.0;
+        String month = scanner.nextLine().toLowerCase();
+        int vacationDays = Integer.parseInt(scanner.nextLine());
 
-        if (month.equals("May") || month.equals("October")) {
-            pricePerNightForStudio = 50 * numberOfNights;
-            pricePerNightForApartment = 65 * numberOfNights;
-            if (14 < numberOfNights) {
-                pricePerNightForStudio = pricePerNightForStudio - (pricePerNightForStudio * 0.3);
-            } else if (7 < numberOfNights) {
-                pricePerNightForStudio = (pricePerNightForStudio * 0.5);
-            }
-        } else if (month.equals("June") || month.equals("September")) {
-            pricePerNightForStudio = 75.20 * numberOfNights;
-            pricePerNightForApartment = 68.70 * numberOfNights;
-            if (numberOfNights > 14) {
-                pricePerNightForStudio = pricePerNightForStudio - (pricePerNightForStudio * 0.2);
-            }
-        } else if (month.equals("July") || month.equals("August")) {
-            pricePerNightForStudio = 76 * numberOfNights;
-            pricePerNightForApartment = 77 * numberOfNights;
+        double studioCost = 0;
+        double apartmentCost = 0;
+
+        switch (month) {
+            case "may":
+            case "october":
+                studioCost = 50 * vacationDays;
+                apartmentCost = 65 * vacationDays;
+                if (vacationDays <= 7) {
+                    studioCost *= 1;
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                } else if (vacationDays <= 14) {
+                    studioCost *= 0.95;
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                } else {
+                    studioCost *= 0.7;
+                    apartmentCost *= 0.9;
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                }
+                break;
+            case "june":
+            case "september":
+                studioCost = 75.2 * vacationDays;
+                apartmentCost = 68.7 * vacationDays;
+                if (vacationDays > 14) {
+                    studioCost *= 0.8;
+                    apartmentCost *= 0.9;
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                } else {
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                }
+                break;
+            case "july":
+            case "august":
+                studioCost = 76 * vacationDays;
+                apartmentCost = 77 * vacationDays;
+                if (vacationDays > 14) {
+                    apartmentCost *= 0.9;
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                } else {
+                    System.out.printf("Apartment: %.2f lv.%n", apartmentCost);
+                    System.out.printf("Studio: %.2f lv.", studioCost);
+                }
+                break;
         }
-        if (numberOfNights > 14) {
-            pricePerNightForApartment = pricePerNightForApartment - (pricePerNightForApartment * 0.1);
-        }
-        System.out.printf("Apartment: %.2f lv.", pricePerNightForApartment);
-        System.out.println();
-        System.out.printf("Studio: %.2f lv.", pricePerNightForStudio);
     }
 }
+
