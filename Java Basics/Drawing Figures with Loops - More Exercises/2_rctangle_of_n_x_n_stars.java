cc69d74a89d62079f7e77eee267eb8e2271b3d08
ceo-py <sfmf@abv.bg>
2022-12-14 13:21:09 +0200

    initial solution

---
+++ Java Basics/Drawing Figures with Loops - More Exercises/2_rctangle_of_n_x_n_stars.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int row = 0; row < number; row++) {
+            for (int col = 0; col < number; col++) {
+                System.out.print("*");
+                if (col == number - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
