219bebb3aceda743493f0a50c2126b888270e52e
ceo-py <sfmf@abv.bg>
2022-12-14 12:38:18 +0200

    initial solution

---
+++ Java Basics/Conditional Statements Advanced - More Exercises/9_numbers_from_1_to_10.java
@@ -0,0 +1,11 @@
+public class NumbersFrom1To1009 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
