d195aab66427c870a9eaa946b5124cc315b214dc
ceo-py <sfmf@abv.bg>
2022-12-14 15:24:02 +0200

    initial solution

--- Java Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/3_travel_agency.java
+++ Java Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/3_travel_agency.java
@@ -1,77 +1,95 @@
 import java.util.Scanner;
 
-public class TravelAgency03 {
+public class Travel_Agency_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String city = scanner.nextLine();
-        String typePackage = scanner.nextLine();
-        String VIPDiscount = scanner.nextLine();
+        String pack = scanner.nextLine();
+        String vip = scanner.nextLine();
         int days = Integer.parseInt(scanner.nextLine());
+        double dayPrice = 0;
 
-
-        if (days > 7) {
-            days--;
-        }
-        double totalSum = 0;
-
-        if (days < 1) {
-            System.out.println("Days must be positive number!");
-        } else {
-            switch (city) {
-                case "Bansko":
-                case "Borovets":
-                    switch (typePackage) {
-                        case "noEquipment":
-                            switch (VIPDiscount) {
-                                case "yes":
-                                    totalSum = days * 80 * 0.95;
-                                    break;
-                                case "no":
-                                    totalSum = days * 80;
-                                    break;
-                            }
-                            break;
-                        case "withEquipment":
-                            switch (VIPDiscount) {
-                                case "yes":
-                                    totalSum = days * 100 * 0.90;
-                                    break;
-                                case "no":
-                                    totalSum = days * 100;
-                                    break;
-                            }
-                    } System.out.printf("The price is %.2flv! Have a nice time!", totalSum);
-                    break;
-                case "Varna":
-                case "Burgas":
-                    switch (typePackage) {
-                        case "noBreakfast":
-                            switch (VIPDiscount) {
-                                case "yes":
-                                    totalSum = days * 100 * 0.93;
-                                    break;
-                                case "no":
-                                    totalSum = days * 100;
-                                    break;
-                            }
-                            break;
-                        case "withBreakfast":
-                            switch (VIPDiscount) {
-                                case "yes":
-                                    totalSum = days * 130 * 0.88;
-                                    break;
-                                case "no":
-                                    totalSum = days * 130;
-                                    break;
-                            }
-                    }
-                    System.out.printf("The price is %.2flv! Have a nice time!", totalSum);
-                    break;
-                default:
-                    System.out.println("Invalid input!");
-                    break;
-            }
+        switch (city) {
+            case "Bansko":
+            case "Borovets":
+                switch (pack) {
+                    case "withEquipment":
+                        dayPrice = 100;
+                        switch (vip) {
+                            case "yes":
+                                dayPrice *= 0.9;
+                                break;
+                        }
+                        if (days > 7) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", (days * dayPrice) - dayPrice);
+                        } else if  (days < 7 && days > 1) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", days * dayPrice);
+                        } else {
+                            System.out.println("Days must be positive number!");
+                        }
+                        break;
+                    case "noEquipment":
+                        dayPrice = 80;
+                        switch (vip) {
+                            case "yes":
+                                dayPrice *= 0.95;
+                                break;
+                        }
+                        if (days > 7) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", (days * dayPrice) - dayPrice);
+                        } else if  (days < 7 && days > 1) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", days * dayPrice);
+                        } else {
+                            System.out.println("Days must be positive number!");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid input!");
+                        break;
+                }
+                break;
+            case "Varna":
+            case "Burgas":
+                switch (pack) {
+                    case "withBreakfast":
+                        dayPrice = 130;
+                        switch (vip) {
+                            case "yes":
+                                dayPrice *= 0.88;
+                                break;
+                        }
+                        if (days > 7) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", (days * dayPrice) - dayPrice);
+                        } else if  (days < 7 && days > 1) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", days * dayPrice);
+                        } else {
+                            System.out.println("Days must be positive number!");
+                        }
+                        break;
+                    case "noBreakfast":
+                        dayPrice = 100;
+                        switch (vip) {
+                            case "yes":
+                                dayPrice *= 0.93;
+                                break;
+                        }
+                        if (days > 7) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", (days * dayPrice) - dayPrice);
+                        } else if  (days < 7 && days > 1) {
+                            System.out.printf("The price is %.2flv! Have a nice time!", days * dayPrice);
+                        } else {
+                            System.out.println("Days must be positive number!");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid input!");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Invalid input!");
+                break;
         }
     }
 }
