f34208e398bea95677492587134e86c59d662175
ceo-py <sfmf@abv.bg>
2022-12-14 10:50:21 +0200

    initial solution

---
+++ Java Basics/While Loop - Lab/5_account_balance.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AccountBalance05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        double sum = 0;
+
+        while (!input.equals("NoMoreMoney")) {
+            double payment = Double.parseDouble(input);
+
+            if (payment < 0) {
+                System.out.println("Invalid operation!");
+                break;
+            }
+            System.out.printf("Increase: %.2f%n", payment);
+            sum += payment;
+            input = scanner.nextLine();
+        }
+        System.out.printf("Total: %.2f", sum);
+    }
+}
