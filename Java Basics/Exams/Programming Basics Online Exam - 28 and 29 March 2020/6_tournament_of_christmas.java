9507fa3500b94e9761014777556996e8ebe86ec2
ceo-py <sfmf@abv.bg>
2022-12-14 15:41:50 +0200

    initial solution

--- Java Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/6_tournament_of_christmas.java
+++ Java Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/6_tournament_of_christmas.java
@@ -1,62 +1,44 @@
 import java.util.Scanner;
 
-public class Tournament_Of_Christmas_06 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        int days = Integer.parseInt(scanner.nextLine());
-        String sport = "";
+    int tournamentDays = scanner.nextInt();
+    int totalWins = 0;
+    double totalMoney = 0;
 
-        int totalWins = 0;
-        int totalLoses = 0;
+    for (int i = 0; i < tournamentDays; i++) {
+      int currentDayWins = 0;
+      double currentDayMoney = 0;
 
-        int end = 0;
-        boolean flag = false;
-
-        int dayWins = 0;
-        int dayLoses = 0;
-        double wonMoneyPerDay = 0;
-        double totalMoney = 0;
-
-        for (int a = 1; a <= days; a++) {
-            totalMoney += wonMoneyPerDay;
-
-            while (!sport.equals("Finish")) {
-                sport = scanner.nextLine();
-
-                if (sport.equals("Finish")) {
-                    end++;
-                    if (dayWins > dayLoses) {
-                        wonMoneyPerDay *= 1.1;
-                    }
-                    if (end >= days) {
-                        flag = true;
-                        break;
-                    }
-                    sport = scanner.nextLine();
-                }
-                String score = scanner.nextLine();
-
-                if (score.equals("win")) {
-                    dayWins++;
-                    totalWins++;
-                    wonMoneyPerDay += 20;
-                }
-                if (score.equals("lose")) {
-                    dayLoses++;
-                    totalLoses++;
-                }
-            }
-            if (flag) {
-                break;
-            }
+      while (true) {
+        String sport = scanner.next();
+        if (sport.equals("Finish")) {
+          totalWins += currentDayWins;
+          if (currentDayWins > 0) {
+            currentDayMoney *= 1.1;
+          }
+          totalMoney += currentDayMoney;
+          break;
         }
 
-        if (totalWins > totalLoses) {
-            double finalMoney = totalMoney * 1.2;
-            System.out.printf("You won the tournament! Total raised money: %.2f", finalMoney);
-        } else {
-            System.out.printf("You lost the tournament! Total raised money: %.2f", totalMoney);
+        String gameOutput = scanner.next();
+
+        if (gameOutput.equals("win")) {
+          currentDayWins += 1;
+          currentDayMoney += 20;
+        } else if (gameOutput.equals("lose")) {
+          currentDayWins -= 1;
         }
+      }
     }
+
+    if (totalWins > 0) {
+      totalMoney *= 1.2;
+      System.out.printf("You won the tournament! Total raised money: %.2f\n", totalMoney);
+    } else {
+      System.out.printf("You lost the tournament! Total raised money: %.2f\n", totalMoney);
+    }
+  }
 }
