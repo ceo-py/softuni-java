b0d4260325d5db11fae1aaa74808073ad5918167
ceo-py <sfmf@abv.bg>
2022-12-14 12:43:13 +0200

    initial solution

---
+++ Java Basics/While-Loop - More Exercises/5_average_nmber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Average_Number_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        double sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            sum += num;
+        }
+
+        System.out.printf("%.2f", sum / n);
+    }
+}
