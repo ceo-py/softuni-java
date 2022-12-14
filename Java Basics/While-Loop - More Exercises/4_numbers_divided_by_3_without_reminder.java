d56d617a875a1a3378fec64d712772e9282ef628
ceo-py <sfmf@abv.bg>
2022-12-14 12:42:34 +0200

    initial solution

---
+++ Java Basics/While-Loop - More Exercises/4_numbers_divided_by_3_without_reminder.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Numbers_Divided_By_3_Without_Reminder_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 3; i <= 99; i += 3) {
+            System.out.println(i);
+        }
+
+    }
+}
