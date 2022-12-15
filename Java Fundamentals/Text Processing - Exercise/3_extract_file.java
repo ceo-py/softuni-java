10ed87b6ccc056b646d93967830ba41298147000
ceo-py <sfmf@abv.bg>
2022-12-15 22:01:31 +0200

    initial solution

---
+++ Java Fundamentals/Text Processing - Exercise/3_extract_file.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Extract_File_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\\\");
+
+        String result = input[input.length - 1];
+        String fileName = result.split("\\.")[0];
+        String extension = result.split("\\.")[1];
+
+        System.out.print("File name: " + fileName);
+        System.out.println();
+        System.out.print("File extension: " + extension);
+    }
+}
