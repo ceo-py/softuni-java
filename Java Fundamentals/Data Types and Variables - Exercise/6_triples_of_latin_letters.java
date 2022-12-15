56fb1837fc208fbb10c600bd757a531f956aa210
ceo-py <sfmf@abv.bg>
2022-12-15 19:54:26 +0200

    initial solution

---
+++ Java Fundamentals/Data Types and Variables - Exercise/6_triples_of_latin_letters.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 'a'; i < 'a' + n; i++) {
+            for (int j = 'a'; j < 'a' + n ; j++) {
+                for (int k = 'a'; k < 'a' + n ; k++) {
+                    System.out.printf("%c%c%c%n", i, j, k);
+                }
+            }
+        }
+    }
+}
