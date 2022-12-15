537ede7bb6ec22d5d1e3e599e35974ebfd215120
ceo-py <sfmf@abv.bg>
2022-12-15 14:05:45 +0200

    initial solution

--- Java Basics/For-Loop - More Exercises/8_equal_pairs.java
+++ Java Basics/For-Loop - More Exercises/8_equal_pairs.java
@@ -1,51 +1,23 @@
 import java.util.Scanner;
 
-public class EqualPairs08 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        int pairs = Integer.parseInt(scanner.nextLine());
+    int count = scanner.nextInt();
+    int previousSum = scanner.nextInt() + scanner.nextInt();
 
-        int sum = 0;
-        int maxDiff = 0;
-        int sumNext = 0;
-        int nextDiff = 0;
-
-        for (int i = 1; i <= pairs; i++) {
-            int number = Integer.parseInt(scanner.nextLine());
-            int nextNumber = Integer.parseInt(scanner.nextLine());
-
-            if (i % 2 == 0) {
-                sumNext = number + nextNumber;
-
-                if (sumNext != sum) {
-                    nextDiff = Math.abs(sum - sumNext);
-                    if (maxDiff < nextDiff) {
-                        maxDiff = nextDiff;
-                    }
-                }
-            } else {
-                sum = number + nextNumber;
-                maxDiff = sum;
-
-            }
-
-//            if (sum1 >= maxSum) {
-//                maxSum = sum;
-//            }
-//            if (sum1 <= minSum) {
-//                minSum = sum;
-        }
-        if (sum == sumNext) {
-            System.out.printf("Yes, value=%d", maxDiff);
-        } else {
-            System.out.printf("No, maxdiff=%d", sum - sumNext);
-        }
+    int maxDifference = 0;
+    for (int i = 1; i < count; i++) {
+      int currentSum = scanner.nextInt() + scanner.nextInt();
+      maxDifference = Math.max(maxDifference, Math.abs(previousSum - currentSum));
+      previousSum = currentSum;
     }
 
+    if (maxDifference == 0) {
+      System.out.println("Yes, value=" + previousSum);
+    } else {
+      System.out.println("No, maxdiff=" + maxDifference);
+    }
+  }
 }
-
-
-
-
-
