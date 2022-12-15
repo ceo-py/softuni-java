64912cb87ffce55fe34d0421625c02d1ddd0a09f
ceo-py <sfmf@abv.bg>
2022-12-15 14:13:18 +0200

    initial solution

--- Java Basics/First Steps in Coding - More Exercises/5_training_lab.java
+++ Java Basics/First Steps in Coding - More Exercises/5_training_lab.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
-public class TrainingLab {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double length = Double.parseDouble(scanner.nextLine());
-        double width = Double.parseDouble(scanner.nextLine());
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        double placesByLength = length / 1.2;
-        int placesByLengthRounded = (int)placesByLength; // double -> int - TypeCasting
-        double widthAdjusted = width - 1;
-        double placesByWidth = widthAdjusted / 0.7;
-        int placesByWidthRounded = (int)Math.round(placesByWidth); //double -> int Math.round method
-        int placesByWidthAndLength = placesByLengthRounded * placesByWidthRounded;
-        int totalPlaces = placesByWidthAndLength - 3;
-        System.out.printf("%d", totalPlaces);
+    float w = scanner.nextFloat();
+    float h = scanner.nextFloat();
 
-    }
+    int chairH = 70;
+    int chairW = 120;
+
+    float totalW = w * 100;
+    float totalH = h * 100;
+
+    float freeRowH = totalH - 100;
+    float chairRowH = (int) freeRowH / chairH;
+
+    float chairRowW = (int) totalW / chairW;
+
+    int allChairs = (int) ((chairRowH * chairRowW) - 3);
+
+    System.out.println(allChairs);
+  }
 }
