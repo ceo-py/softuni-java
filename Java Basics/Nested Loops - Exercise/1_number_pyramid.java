3bc3c93600fc13ef67c50f9ea8c57a0fc4b2e1e8
ceo-py <sfmf@abv.bg>
2022-12-15 14:26:27 +0200

    initial solution

--- Java Basics/Nested Loops - Exercise/1_number_pyramid.java
+++ Java Basics/Nested Loops - Exercise/1_number_pyramid.java
@@ -1,23 +1,29 @@
+package com.company;
+
 import java.util.Scanner;
 
-public class NumberPyramid01 {
+public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
-        int currentNumber = 1;
-        for (int rows = 1; rows <= number ; rows++) {
-            for (int columns = 1; columns <= number ; columns++) {
-                if (currentNumber > number) {
+        int counter = 0;
+        boolean isEqual = false;
+        for (int rows = 1; rows <= number; rows++) {//
+            for (int cols = 1; cols <= rows; cols++) {//
+                counter++;
+                if (counter > number) {
+                    isEqual = true;
                     break;
                 }
-                System.out.println(currentNumber + " ");
-                currentNumber++;
-            }
-            if (currentNumber > number) {
-                break;
+                System.out.printf(counter + " ");
             }
             System.out.println();
+            if (isEqual) {
+                break;
+            }
         }
+
     }
 }
