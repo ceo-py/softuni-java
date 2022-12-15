a58e5da271a87f9eab76d39ef6de9f42bdbc7528
ceo-py <sfmf@abv.bg>
2022-12-15 22:48:49 +0200

    initial solution

---
+++ Java Fundamentals/Exams/05. Programming Fundamentals Mid Exam/1_bonus_scoring_system.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bonus_Scoring_System_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int studentsCount = Integer.parseInt(scanner.nextLine());
+        double lecturesCount = Double.parseDouble(scanner.nextLine());
+        double additionalBonus = Double.parseDouble(scanner.nextLine());
+        double maxBonus = 0;
+        double studentAttendances = 0;
+
+        for (int i = 1; i <= studentsCount; i++) {
+            int attendancesForStudent = Integer.parseInt(scanner.nextLine());
+            double totalBonus = 0;
+            totalBonus = (attendancesForStudent / lecturesCount) * (5 + additionalBonus);
+
+            if (totalBonus >= maxBonus) {
+                maxBonus = totalBonus;
+                studentAttendances = attendancesForStudent;
+            }
+        }
+
+        System.out.printf("Max Bonus: %d.%n", (int) Math.ceil(maxBonus));
+        System.out.printf("The student has attended %d lectures.", (int) studentAttendances);
+    }
+}
