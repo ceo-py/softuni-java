6429265df0592bdb374c1017987b7ff94d3357a1
ceo-py <sfmf@abv.bg>
2022-12-15 23:14:18 +0200

    initial solution

--- Java Fundamentals/Exams/03. Programming Fundamentals Final Exam Retake/3_need_for_speed_iii.java
+++ Java Fundamentals/Exams/03. Programming Fundamentals Final Exam Retake/3_need_for_speed_iii.java
@@ -1,91 +1,83 @@
 import java.util.*;
 
-public class Need_For_Speed_III_03 {
+public class NeedForSpeedIII03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int numberOfCars = Integer.parseInt(scanner.nextLine());
+        int numberCars = Integer.parseInt(scanner.nextLine());
 
-        Map<String, List<Integer>> cars = new HashMap<>();
+        Map<String, List<Integer>> carDataMap = new LinkedHashMap<>();
 
-        for (int i = 0; i < numberOfCars; i++) {
-            String currentCar = scanner.nextLine();
-            String [] currentCarArray = currentCar.split("\\|");
-            String currentCarModel = currentCarArray[0];
-            int mileage = Integer.parseInt(currentCarArray[1]);
-            int fuel = Integer.parseInt(currentCarArray[2]);
+        for (int i = 1; i <= numberCars; i++) {
+            String[] carData = scanner.nextLine().split("\\|");
+            String car = carData[0];
+            int mileage = Integer.parseInt(carData[1]);
+            int fuel = Integer.parseInt(carData[2]);
 
-            cars.put(currentCarModel, Arrays.asList(mileage, fuel));
+            carDataMap.putIfAbsent(car, new ArrayList<>());
+            carDataMap.get(car).add(mileage);
+            carDataMap.get(car).add(fuel);
         }
+        String commandLine = scanner.nextLine();
 
-        String command = scanner.nextLine();
-        while (!command.equals("Stop")) {
-            String [] commandArray = command.split(" : ");
-            String commandName = commandArray[0];
-            String car = commandArray[1];
+        while (!commandLine.equals("Stop")) {
+            String[] commandData = commandLine.split(" : ");
+            String command = commandData[0];
+            String car = commandData[1];
 
-            switch (commandName) {
+            switch (command) {
                 case "Drive":
-                    int distance = Integer.parseInt(commandArray[2]);
-                    int fuel = Integer.parseInt(commandArray[3]);
-
-                    if (fuel <= cars.get(car).get(1)) {
-                        int newDistance = cars.get(car).get(0) + distance;
-                        int newFuel = cars.get(car).get(1) - fuel;
-                        cars.put(car, Arrays.asList(newDistance, newFuel));
-                        System.out.printf("%s driven for %d kilometers. %d liters of fuel consumed.%n", car, distance, fuel);
-
-                        if (cars.get(car).get(0) > 100000) {
-                            System.out.printf("Time to sell the %s!%n", car);
-                            cars.remove(car);
-                        }
+                    int distance = Integer.parseInt(commandData[2]);
+                    int fuelForDistance = Integer.parseInt(commandData[3]);
+                    if (fuelForDistance > carDataMap.get(car).get(1)) {
+                        System.out.println("Not enough fuel to make that ride");
                     } else {
-                        System.out.printf("Not enough fuel to make that ride%n");
+                        int newMileage = carDataMap.get(car).get(0) + distance;
+                        carDataMap.get(car).remove(0);
+                        carDataMap.get(car).add(0, newMileage);
+
+
+                        int newFuel = carDataMap.get(car).get(1) - fuelForDistance;
+                        carDataMap.get(car).remove(1);
+                        carDataMap.get(car).add(1, newFuel);
+                        System.out.printf("%s driven for %d kilometers. %d liters of fuel consumed.%n", car, distance, fuelForDistance);
+                        if (carDataMap.get(car).get(0) >= 100000) {
+                            System.out.printf("Time to sell the %s!%n", car);
+                            carDataMap.remove(car);
+                            break;
+                        }
                     }
                     break;
                 case "Refuel":
-                    int fuelForRefuel = Integer.parseInt(commandArray[2]);
-                    int newFuel = cars.get(car).get(1) + fuelForRefuel;
-
-                    if (newFuel <= 75) {
-                        cars.get(car).set(1, newFuel);
-                        System.out.printf("%s refueled with %d liters%n", car, fuelForRefuel);
-                    } else {
-                        int fuel1 = (newFuel - 75) -fuelForRefuel;
-                        System.out.printf("%s refueled with %d liters%n", car, Math.abs(fuel1));
-                        cars.get(car).set(1, 75);
+                    int refill = Integer.parseInt(commandData[2]);
+                    if (refill + carDataMap.get(car).get(1) > 75) {
+                        refill = 75 - carDataMap.get(car).get(1);
                     }
+                    int newFuelAmount = carDataMap.get(car).get(1) + refill;
+                    carDataMap.get(car).remove(1);
+                    carDataMap.get(car).add(1, newFuelAmount);
+                    System.out.printf("%s refueled with %d liters%n", car, refill);
                     break;
                 case "Revert":
-                    int km = Integer.parseInt(commandArray[2]);
-                    int newKm = cars.get(car).get(0) - km;
-
-                    if (newKm < 10000) {
-                        cars.get(car).set(0, 10000);
+                    int kilometres = Integer.parseInt(commandData[2]);
+                    int decreasedMileage = carDataMap.get(car).get(0) - kilometres;
+                    carDataMap.get(car).remove(0);
+                    carDataMap.get(car).add(0, decreasedMileage);
+                    if (carDataMap.get(car).get(0) >= 10000) {
+                        System.out.printf("%s mileage decreased by %d kilometers%n", car, kilometres);
                     } else {
-                        cars.get(car).set(0, newKm);
-                        System.out.printf("%s mileage decreased by %d kilometers%n", car, km);
+                        carDataMap.get(car).remove(0);
+                        carDataMap.get(car).add(0, 10000);
                     }
                     break;
+                default:
+                    break;
+
             }
 
-            command = scanner.nextLine();
+            commandLine = scanner.nextLine();
         }
 
-        cars
-            .entrySet()
-            .stream()
-            .sorted((a, b) -> {
-                int result = b.getValue().get(0).compareTo(a.getValue().get(0));
-                if (result == 0) {
-                    result = a.getKey().compareTo(b.getKey());
-                }
-                return result;
-            })
-            .forEach(entry -> System.out.printf("%s -> Mileage: %d kms, Fuel in the tank: %d lt.%n",
-                                                entry.getKey(),
-                                                entry.getValue().get(0),
-                                                entry.getValue().get(1)));
-
+        carDataMap.forEach((k, v) -> System.out.printf("%s -> Mileage: %d kms, Fuel in the tank: %d lt.%n", k, v.get(0), v.get(1)));
     }
 }
