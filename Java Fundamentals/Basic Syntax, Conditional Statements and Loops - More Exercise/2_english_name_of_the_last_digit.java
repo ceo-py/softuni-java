ce9bac7a5d87a72246c42257cf571c472e1bbc20
ceo-py <sfmf@abv.bg>
2022-12-15 19:35:39 +0200

    initial solution

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/2_english_name_of_the_last_digit.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class English_Name_Of_The_Last_Digit_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int lastNum = number % 10;
+
+        switch (lastNum) {
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+        }
+
+    }
+}
