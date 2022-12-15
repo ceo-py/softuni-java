b66d3e2c87939a6ec70e9fa35a0538ed5c177529
ceo-py <sfmf@abv.bg>
2022-12-15 20:59:04 +0200

    initial solution

---
+++ Java Fundamentals/Objects and Classes - Lab/1_randomize_words.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Randomize_Words_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] sentence = scanner.nextLine().split(" ");
+
+        Random random = new Random();
+        for (int i = 0; i < sentence.length; i++) {
+            int randomIndex = random.nextInt(sentence.length);
+            String temp = sentence[i];
+            sentence[i] = sentence[randomIndex];
+            sentence[randomIndex] = temp;
+        }
+
+        for (String word : sentence) {
+            System.out.println(word);
+        }
+
+    }
+}
+Footer
+© 2022 G
