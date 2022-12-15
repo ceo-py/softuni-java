ebf27652131dbcd17019ea9494ac3f9e10e75c4e
ceo-py <sfmf@abv.bg>
2022-12-15 19:20:17 +0200

    initial solution

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/11_multiplication_table_2_0.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Multiplication_Table_20_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        do {
+            System.out.printf("%d X %d = %d%n", num1, num2, num1 * num2);
+            num2++;
+        }
+        while (num2 <= 10);
+
+    }
+}
