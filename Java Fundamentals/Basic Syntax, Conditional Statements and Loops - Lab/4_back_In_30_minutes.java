61ba64e8a19574df85e74d13b80aae00664122a2
ceo-py <sfmf@abv.bg>
2022-12-15 19:05:40 +0200

    initial solution

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/4_back_In_30_minutes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Back_In_30_Minutes_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        minutes = minutes + 30;
+        if (minutes > 60) {
+            hours++;
+            minutes = minutes - 60;
+        }
+
+        if (hours > 23) {
+            hours = 0;
+        }
+
+        System.out.printf("%d:%02d", hours, minutes);
+    }
+}
