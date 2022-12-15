42c1641e5ee80809b810410409b262940a35a477
ceo-py <sfmf@abv.bg>
2022-12-15 20:00:23 +0200

    initial solution

---
+++ Java Fundamentals/Data Types and Variables - More Exercise/4_refactoring_prime_checker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Refactoring_Prime_Checker_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+            for (int cepitel = 2; cepitel < i; cepitel++) {
+                if (i % cepitel == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            System.out.printf("%d -> %s%n", i, isPrime);
+        }
+
+    }
+}
