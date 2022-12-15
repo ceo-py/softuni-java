77679ca22e9e3c2c3bc73ca441d3b10da8ac64d4
ceo-py <sfmf@abv.bg>
2022-12-15 19:45:25 +0200

    initial solution

---
+++ Java Fundamentals/Data Types and Variables - Lab/3_exact_sum_of_real_numbers.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ExactSumOfRealNumbers03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = new BigDecimal("0");
+
+        for (int i = 1; i <= n ; i++) {
+            BigDecimal current = new BigDecimal(scanner.nextLine());
+            sum = sum.add(current);
+        }
+        System.out.println(sum);
+    }
+}
