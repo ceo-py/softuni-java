28384fc55d47801a6b218a8a7b14b72e7631984e
ceo-py <sfmf@abv.bg>
2022-12-15 21:06:11 +0200

    initial solution

--- Java Fundamentals/Objects and Classes - Lab/5_students.java
+++ Java Fundamentals/Objects and Classes - Lab/5_students.java
@@ -34,3 +34,37 @@ public class Main {
 
     }
 }
+
+
+package Students_05;
+
+public class Students {
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final String hometown;
+
+    public Students(String firstName, String lastName, int age, String hometown) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    public String getHomeTown () {
+        return this.hometown;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
