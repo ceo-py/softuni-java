c491fbd6d0dc869c24ca0b57ec642b6614499403
ceo-py <sfmf@abv.bg>
2022-12-15 19:07:34 +0200

    initial solution

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/6_foreign_languages.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Foreign_Languages_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+
+    }
+}
+Footer
+© 2022 Git
