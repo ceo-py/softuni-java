e587082deb2b38762013311d227b22eff4044c7e
ceo-py <sfmf@abv.bg>
2022-12-15 19:27:09 +0200

    initial solution

---
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/4_print_and_sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintAndSum04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = start; i <= end ; i++) {
+            System.out.printf("%d ", i);
+            sum += i;
+        }
+        System.out.println("");
+        System.out.printf("Sum: %d", sum);
+    }
+}
