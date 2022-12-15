9033e8cb4e2a77f6d274b9ba7d11a80fb39e4f28
ceo-py <sfmf@abv.bg>
2022-12-15 22:00:11 +0200

    initial solution

---
+++ Java Fundamentals/Text Processing - Exercise/1_valid_usernames.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Valid_Usernames_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String[] usernames = text.split(", ");
+
+        for (String username : usernames) {
+            if(isValidUsername(username)) {
+                System.out.println(username);
+            }
+        }
+    }
+
+    static boolean isValidUsername (String username) {
+        if (username.length() < 3 || username.length() > 16) {
+            return false;
+        }
+
+        for (int index = 0; index <= username.length() - 1; index++) {
+            char currentSymbol = username.charAt(index);
+            if(!Character.isLetterOrDigit(currentSymbol) && currentSymbol != '-' && currentSymbol != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
