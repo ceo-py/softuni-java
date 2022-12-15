09947e811bda5ca3e5ecf1e0ce9c7ff0ad61e28b
ceo-py <sfmf@abv.bg>
2022-12-15 22:02:14 +0200

    initial solution

---
+++ Java Fundamentals/Text Processing - Exercise/4_caesar_cipher.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Caesar_Cipher_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i <= input.length() - 1; i++) {
+            char letter = input.charAt(i);
+            letter += 3;
+
+            output.append(letter);
+        }
+
+        System.out.println(output.toString());
+    }
+}
