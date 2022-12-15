e5e7354c222a8fbc54a42df8523ad3f92c6630b8
ceo-py <sfmf@abv.bg>
2022-12-15 20:54:15 +0200

    initial solution

--- Java Fundamentals/Lists - Exercise/9_pokemon_dont_go.java
+++ Java Fundamentals/Lists - Exercise/9_pokemon_dont_go.java
@@ -3,51 +3,39 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class PokemonDontGo09 {
+public class Pokemon_Dont_Go_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> spaceList = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int index = Integer.parseInt(scanner.nextLine());
-        int removedSum = 0;
-        while (spaceList.size() > 0) {
+        List<Integer> inputNumbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        int sum = 0;
+
+        while (!inputNumbers.isEmpty()) {
+            int index = Integer.parseInt(scanner.nextLine());
+            int value = 0;
 
             if (index < 0) {
-                int toRemove = spaceList.get(0);
-                spaceList.remove(0);
-                removedSum += toRemove;
-                int lastIndexValue = spaceList.get(spaceList.size() - 1);
-                spaceList.add(0, lastIndexValue);
-            } else if (index > spaceList.size() - 1) {
-                int toRemove = spaceList.get(spaceList.size() - 1);
-                removedSum += toRemove;
-                spaceList.remove(spaceList.size() - 1);
-                int firstIndexValue = spaceList.get(0);
-                spaceList.add(spaceList.size() - 1, firstIndexValue);
+                value = inputNumbers.get(0);
+                inputNumbers.set(0, inputNumbers.get(inputNumbers.size() - 1));
+            } else if (index >= inputNumbers.size()) {
+                value = inputNumbers.get(inputNumbers.size() - 1);
+                inputNumbers.set(inputNumbers.size() - 1, inputNumbers.get(0));
             } else {
-                int currentRemoved = spaceList.get(index);
-                int toRemove = spaceList.get(index);
-                removedSum += toRemove;
-                spaceList.remove(index);
-                for (int i = 0; i < spaceList.size(); i++) {
-                    if (currentRemoved >= spaceList.get(i)) {
-                        int current = spaceList.get(i);
-                        spaceList.add(i,current + currentRemoved);
-                        spaceList.remove(i + 1);
-                    } else {
-                        int current = spaceList.get(i);
-                        spaceList.add(i,current - currentRemoved);
-                        spaceList.remove(i + 1);
-                    }
+                value = inputNumbers.remove(index);
+            }
 
+            sum += value;
+
+            for (int i = 0; i < inputNumbers.size(); i++) {
+                if (inputNumbers.get(i) <= value) {
+                    inputNumbers.set(i, inputNumbers.get(i) + value);
+                } else {
+                    inputNumbers.set(i, inputNumbers.get(i) - value);
                 }
             }
-            if (spaceList.size() == 0) {
-                break;
-            }
-
-            index = Integer.parseInt(scanner.nextLine());
         }
-        System.out.println(removedSum);
+
+        System.out.println(sum);
     }
 }
