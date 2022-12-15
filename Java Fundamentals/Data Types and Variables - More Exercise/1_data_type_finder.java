cb18e87e12df6d50d7b9aa542abfa5d9de03b7cc
ceo-py <sfmf@abv.bg>
2022-12-15 19:58:19 +0200

    initial solution

---
+++ Java Fundamentals/Data Types and Variables - More Exercise/1_data_type_finder.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Data_Type_Finder_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+            String type = "";
+
+            if (input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false")) { // boolean
+                type = "boolean";
+            } else if (input.length() == 1) { // char; int
+                char symbol = input.charAt(0);
+                if (symbol < 47 || symbol > 58) {
+                    type = "character";
+                } else {
+                    type = "integer";
+                }
+            } else { // string; double; int
+                boolean isString = false;
+                boolean isFloat = false;
+
+                for (int i = 0; i < input.length(); i++) {
+                    char currentSymbol = input.charAt(i);
+                    if (currentSymbol < 45 || currentSymbol > 57) {
+                        isString = true;
+                    }
+
+                    if (currentSymbol == 46) {
+                        isFloat = true;
+                    }
+                }
+
+                if (isString) {
+                    type = "string";
+                } else {
+                    if (isFloat) {
+                        type = "floating point";
+                    } else {
+                        type = "integer";
+                    }
+                }
+            }
+            System.out.printf("%s is %s type%n", input, type);
+
+            input = scanner.nextLine();
+        }
+
+    }
+}
