39f64036ba232d4eb2eecf2f1ebfcba7ca2df025
ceo-py <sfmf@abv.bg>
2022-12-25 22:51:38 +0200

    initial solution

---
+++ Java Advanced/Iterators and Comparators - Exercises/5 Comparing Objects/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        List<Person> people = new ArrayList<>();
+
+        while (!input.equals("END")) {
+            String[] peopleData = input.split(" ");
+            String name = peopleData[0];
+            int age = Integer.parseInt(peopleData[1]);
+            String town = peopleData[2];
+
+            Person person = new Person(name, age, town);
+            people.add(person);
+            input = scanner.nextLine();
+        }
+        int comparePersonIndex = Integer.parseInt(scanner.nextLine());
+        Person personToCompare = people.get(comparePersonIndex - 1);
+
+        int totalPeople = people.size();
+        int samePeople = 0;
+        int differentPeople = 0;
+
+        for (Person person : people) {
+            if (person.compareTo(personToCompare) == 0) {
+                samePeople++;
+            } else {
+                differentPeople++;
+            }
+        }
+        if (samePeople == 1) {
+            System.out.println("No matches");
+        } else {
+            System.out.printf("%d %d %d", samePeople, differentPeople, totalPeople);
+        }
+    }
+}
