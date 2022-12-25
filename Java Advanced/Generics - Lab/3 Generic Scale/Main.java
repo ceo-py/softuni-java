34f469146a15a5c50b808089b201c2ee86490894
ceo-py <sfmf@abv.bg>
2022-12-25 22:04:22 +0200

    initial solution

---
+++ Java Advanced/Generics - Lab/3 Generic Scale/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Scale<String> scale = new Scale<>("A", "B");
+
+        System.out.println(scale.getHeavier());
+    }
+}
