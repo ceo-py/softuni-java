a279703c8f38c6993b9a0a35756fc264274ddca6
ceo-py <sfmf@abv.bg>
2022-12-25 22:10:54 +0200

    initial solution

---
+++ Java Advanced/Generics - Exercises/2 Generic Box of Integer/Box.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> { //T -> String
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (T value : values) {
+            stringBuilder.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+
+        return stringBuilder.toString();
+    }
+}
