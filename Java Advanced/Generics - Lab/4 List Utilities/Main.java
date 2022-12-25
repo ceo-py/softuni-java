baa6c2ae66a96416e38770b30769e46930ee769e
ceo-py <sfmf@abv.bg>
2022-12-25 22:06:11 +0200

    initial solution

---
+++ Java Advanced/Generics - Lab/4 List Utilities/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(13, 42, 69, 73);
+
+        System.out.println(ListUtils.getMin(numbers));
+        System.out.println(ListUtils.getMax(numbers));
+    }
+}
