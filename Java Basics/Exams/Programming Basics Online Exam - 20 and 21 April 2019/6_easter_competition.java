25f2fb04a5f5c40b8d4dfedf81e7ff42f5993df0
ceo-py <sfmf@abv.bg>
2023-10-21 13:27:49 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/6_easter_competition.java
+++ Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/6_easter_competition.java
@@ -30,3 +30,44 @@ public class EasterCompetition06 {
         System.out.printf("%s won competition with %d points!%n", winner, maxPoints);
     }
 }
+
+
+//
+// import java.util.Scanner;
+//
+// public class EasterCompetition_06 {
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int easterBreads = Integer.parseInt(scanner.nextLine());
+//
+//         int maxPoints = 0;
+//         String winner = "";
+//
+//         for (int count = 1; count <= easterBreads; count++) {
+//
+//             String bakerName = scanner.nextLine();
+//             int totalPoints = 0;
+//
+//             String input = scanner.nextLine(); // "Stop" или число
+//
+//             while (!input.equals("Stop")) {
+//
+//                 int points = Integer.parseInt(input); // превръщаме низа в цяло число
+//                 totalPoints += points;
+//
+//                 input = scanner.nextLine();
+//             }
+//             System.out.printf("%s has %d points.%n", bakerName, totalPoints);
+//
+//             if (totalPoints > maxPoints) {
+//                 maxPoints = totalPoints;
+//                 winner = bakerName;
+//                 System.out.printf("%s is the new number 1!%n", bakerName);
+//             }
+//
+//         }
+//         System.out.printf("%s won competition with %d points!%n", winner, maxPoints);
+//     }
+// }
