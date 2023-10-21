695d043db8a499d73b0601749cc76971ffacd389
ceo-py <sfmf@abv.bg>
2023-10-21 13:09:56 +0300

    another solution

--- Java Basics/Conditional Statements - Exercise/1_sum_seconds.java
+++ Java Basics/Conditional Statements - Exercise/1_sum_seconds.java
@@ -21,3 +21,32 @@ public class Main {
         }
     }
 }
+
+
+
+
+// import java.util.Scanner;
+//
+// public class _01_SumSeconds {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int first = Integer.parseInt(scanner.nextLine());
+//         int second = Integer.parseInt(scanner.nextLine());
+//         int third = Integer.parseInt(scanner.nextLine());
+//
+//         int sumSeconds = first + second + third;
+//
+//         int minutes = sumSeconds / 60;
+//         int seconds = sumSeconds % 60;
+//
+//         // минути:секунди
+//         if (seconds < 10) {
+//             System.out.printf("%d:0%d", minutes, seconds);
+//         } else {
+//             System.out.printf("%d:%d", minutes, seconds);
+//         }
+//     }
+// }
