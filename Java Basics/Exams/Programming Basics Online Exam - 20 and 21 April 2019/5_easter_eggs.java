1cca37f7e6b8d8cb5508cd6d8b532fc3ef21a04a
ceo-py <sfmf@abv.bg>
2023-10-21 13:27:24 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/5_easter_eggs.java
+++ Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/5_easter_eggs.java
@@ -54,3 +54,67 @@ public class EasterEggs05 {
         System.out.printf("Max eggs: %d -> %s", eggsMax, eggWinner);
     }
 }
+
+
+
+
+// import java.util.Scanner;
+//
+// public class EasterBake_05 {
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int eggs = Integer.parseInt(scanner.nextLine());
+//
+//         int redEgg = 0;
+//         int orangeEgg = 0;
+//         int blueEgg = 0;
+//         int greenEgg = 0;
+//
+//         int maxEggs = 0;
+//         String winner = "";
+//
+//         for (int i = 0; i < eggs; i++) {
+//             String colour = scanner.nextLine();
+//
+//             switch (colour) {   // if (colour.equals("red")) { redEgg++;
+//                 case "red":
+//                     redEgg++;
+//                     if (redEgg >= maxEggs) {
+//                         maxEggs = redEgg;
+//                         winner = colour;
+//                     }
+//                     break;
+//                 case "orange":
+//                     orangeEgg++;
+//                     if (orangeEgg >= maxEggs) {
+//                         maxEggs = orangeEgg;
+//                         winner = colour;
+//                     }
+//                     break;
+//                 case "blue":
+//                     blueEgg++;
+//                     if (blueEgg >= maxEggs) {
+//                         maxEggs = blueEgg;
+//                         winner = colour;
+//                     }
+//                     break;
+//                 case "green":
+//                     greenEgg++;
+//                     if (greenEgg >= maxEggs) {
+//                         maxEggs = greenEgg;
+//                         winner = colour;
+//                     }
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         }
+//         System.out.printf("Red eggs: %d%n", redEgg);
+//         System.out.printf("Orange eggs: %d%n", orangeEgg);
+//         System.out.printf("Blue eggs: %d%n", blueEgg);
+//         System.out.printf("Green eggs: %d%n", greenEgg);
+//         System.out.printf("Max eggs: %d -> %s%n", maxEggs, winner);
+//     }
+// }
