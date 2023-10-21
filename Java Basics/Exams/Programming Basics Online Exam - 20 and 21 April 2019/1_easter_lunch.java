592ffebf5294a116caf7ccfc7d4d49b2924e5ad0
ceo-py <sfmf@abv.bg>
2023-10-21 13:25:37 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/1_easter_lunch.java
+++ Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/1_easter_lunch.java
@@ -13,3 +13,22 @@ public class EasterLunch01 {
         System.out.printf("%.2f", totalCost);
     }
 }
+
+
+//
+// import java.util.Scanner;
+//
+// public class EasterLunch_01 {
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int easterBread = Integer.parseInt(scanner.nextLine());
+//         int eggCarton = Integer.parseInt(scanner.nextLine());
+//         int cookieKg = Integer.parseInt(scanner.nextLine());
+//
+//         double totalPrice = easterBread * 3.20 + eggCarton * 4.35 + cookieKg * 5.40 + eggCarton * 12 * 0.15;
+//
+//         System.out.printf("%.2f", totalPrice);
+//     }
+// }
