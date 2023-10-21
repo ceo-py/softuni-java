c73fd0470b07b6d36668e837bd369a599b4f5982
ceo-py <sfmf@abv.bg>
2023-10-21 13:26:53 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_eggs_battle.java
+++ Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_eggs_battle.java
@@ -33,3 +33,42 @@ public class EasterEggsBattle04 {
         }
     }
 }
+
+
+
+
+// import java.util.Scanner;
+//
+// public class EasterEggsBattle_04 {
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int playerOneEggs = Integer.parseInt(scanner.nextLine());
+//         int playerTwoEggs = Integer.parseInt(scanner.nextLine());
+//         String winner = scanner.nextLine();
+//
+//         while (!winner.equals("End")) {
+//
+//             if (winner.equals("one")) {
+//                 playerTwoEggs -= 1;
+//                 if (playerTwoEggs == 0) {
+//                     System.out.printf("Player two is out of eggs. Player one has %d eggs left.", playerOneEggs);
+//                     return; // прекратява изпълнението на метода/програмата.
+//                 }
+//             } else if (winner.equals("two")) {
+//                 playerOneEggs--; //post-fix decrementation, идентично с playerOneEggs -= 1;
+//                 if (playerOneEggs == 0) {
+//                     System.out.printf("Player one is out of eggs. Player two has %d eggs left.", playerTwoEggs);
+//                     return; // прекратява изпълнението на метода/програмата.
+//                 }
+//             }
+//
+//             winner = scanner.nextLine();
+//         }
+//             // Принтираме, само и единствено ако няма победител.
+//             System.out.printf("Player one has %d eggs left.%n", playerOneEggs);
+//             System.out.printf("Player two has %d eggs left.%n", playerTwoEggs);
+//
+//     }
+// }
