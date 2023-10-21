f42eaf834bf65afd08a58736efe8928d7c61aa4a
ceo-py <sfmf@abv.bg>
2023-10-21 13:15:11 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/3_mobile_operator.java
+++ Java Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/3_mobile_operator.java
@@ -177,3 +177,75 @@ public class MobileOperator03 {
 //         System.out.printf("%.2f lv.", totalSum);
 //     }
 // }
+
+
+
+
+
+// import java.util.Scanner;
+//
+// public class MobileOperator {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         String contractPeriod = scanner.nextLine();
+//         String contractType = scanner.nextLine();
+//         boolean isInternetIncluded = scanner.nextLine().equals("yes");
+//         int monthPeriod = Integer.parseInt(scanner.nextLine());
+//
+//         double monthlyPrice = 0.00;
+//
+//         if (contractPeriod.equals("one")) {
+//             switch (contractType) {
+//                 case "Small":
+//                     monthlyPrice = 9.98;
+//                     break;
+//                 case "Middle":
+//                     monthlyPrice = 18.99;
+//                     break;
+//                 case "Large":
+//                     monthlyPrice = 25.98;
+//                     break;
+//                 case "ExtraLarge":
+//                     monthlyPrice = 35.99;
+//                     break;
+//             }
+//         } else {
+//             switch (contractType) {
+//                 case "Small":
+//                     monthlyPrice = 8.58;
+//                     break;
+//                 case "Middle":
+//                     monthlyPrice = 17.09;
+//                     break;
+//                 case "Large":
+//                     monthlyPrice = 23.59;
+//                     break;
+//                 case "ExtraLarge":
+//                     monthlyPrice = 31.79;
+//                     break;
+//             }
+//         }
+//
+//         if (isInternetIncluded) {
+//
+//             if (monthlyPrice <= 10.00) {
+//                 monthlyPrice = monthlyPrice + 5.50;
+//             } else if (monthlyPrice <= 30.00) {
+//                 monthlyPrice = monthlyPrice + 4.35;
+//             } else {
+//                 monthlyPrice = monthlyPrice + 3.85;
+//             }
+//         }
+//
+//         double totalSum = monthlyPrice * monthPeriod;
+//
+//         if (contractPeriod.equals("two")){
+//             totalSum = totalSum - (totalSum * (3.75 / 100.0));
+//         }
+//
+//         System.out.printf("%.2f lv.", totalSum);
+//     }
+// }
