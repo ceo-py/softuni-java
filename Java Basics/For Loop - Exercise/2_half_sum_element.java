6c420fb149aa37b1e8ab11f92ceddd39fd4b26ec
ceo-py <sfmf@abv.bg>
2023-10-21 13:16:32 +0300

    another solution

--- Java Basics/For Loop - Exercise/2_half_sum_element.java
+++ Java Basics/For Loop - Exercise/2_half_sum_element.java
@@ -28,3 +28,43 @@ public class Main {
         }
     }
 }
+
+
+
+// import java.util.Scanner;
+//
+// public class _02_HalfSumElement {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int n = Integer.parseInt(scanner.nextLine());
+//
+//         int maxNumber = Integer.MIN_VALUE;
+//         int sum = 0;
+//
+//         // Начало: 1
+//         // Край: n
+//         // Стъпка: +1 (i++)
+//         for (int i = 1; i <= n; i++) {
+//
+//             int currentNumber = Integer.parseInt(scanner.nextLine());
+//             sum += currentNumber;
+//
+//             if (maxNumber < currentNumber) {
+//                 maxNumber = currentNumber;
+//             }
+//         }
+//
+//         int sumOther = sum - maxNumber;
+//
+//         if (maxNumber == sumOther) {
+//             System.out.println("Yes");
+//             System.out.printf("Sum = %d", maxNumber);
+//         } else {
+//             System.out.println("No");
+//             System.out.printf("Diff = %d", Math.abs(maxNumber - sumOther));
+//         }
+//     }
+// }
