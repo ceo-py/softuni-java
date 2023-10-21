14da9909afad579c7b19ae4f5f92fc52c2601130
ceo-py <sfmf@abv.bg>
2023-10-21 13:13:27 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/2_movie_day.java
+++ Java Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/2_movie_day.java
@@ -17,3 +17,34 @@ public class MovieDay02 {
         }
     }
 }
+
+
+
+
+// import java.util.Scanner;
+//
+// public class MovieDay {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         // 1. Прочитаме входните данни
+//         int shootingTime = Integer.parseInt(scanner.nextLine());
+//         int sceneCount = Integer.parseInt(scanner.nextLine());
+//         int sceneDuration = Integer.parseInt(scanner.nextLine());
+//
+//         // 2. Изчисляваме подготовката на терен -> 15% от времето за снимки
+//         double preparation = shootingTime * 0.15;
+//
+//         // 3. Намираме общото време за снимки
+//         double totalTime = (sceneCount * sceneDuration) + preparation;
+//
+//         // 4. Проверяваме дали времето e достатъчно
+//         if (shootingTime >= totalTime) {
+//             System.out.printf("You managed to finish the movie on time! You have %d minutes left!", Math.round(shootingTime - totalTime));
+//         } else {
+//             System.out.printf("Time is up! To complete the movie you need %d minutes.", Math.round(totalTime - shootingTime));
+//         }
+//     }
+// }
