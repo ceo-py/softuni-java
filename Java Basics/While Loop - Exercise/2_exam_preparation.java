16bb21eb6cbc114e3926c7f346eb2506d22dc438
ceo-py <sfmf@abv.bg>
2023-10-21 13:20:53 +0300

    another solution

--- Java Basics/While Loop - Exercise/2_exam_preparation.java
+++ Java Basics/While Loop - Exercise/2_exam_preparation.java
@@ -41,3 +41,50 @@ public class ExamPreparation02 {
         }
     }
 }
+
+
+
+// import java.util.Scanner;
+//
+// public class ExamPreparation {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int failedTaskAvailability = Integer.parseInt(scanner.nextLine());
+//
+//         int failedTasks = 0;
+//         int totalTasks = 0;
+//         double totalGrades = 0.00;
+//         String lastProblem = "";
+//
+//         // Начало:
+//         String taskName = scanner.nextLine();
+//
+//         // Условие за край:
+//         while (!taskName.equals("Enough")) {
+//
+//             int mark = Integer.parseInt(scanner.nextLine());
+//             totalTasks++;
+//             totalGrades += mark;
+//
+//             if (mark <= 4) {
+//                 failedTasks++;
+//                 if (failedTasks == failedTaskAvailability) {
+//                     System.out.printf("You need a break, %d poor grades.", failedTasks);
+//                     return;
+//                 }
+//             }
+//
+//             lastProblem = taskName;
+//
+//             // Стъпка напред (прочитаме следващата задача):
+//             taskName = scanner.nextLine();
+//         }
+//
+//         System.out.printf("Average score: %.2f\n", totalGrades / totalTasks);
+//         System.out.printf("Number of problems: %d\n", totalTasks);
+//         System.out.printf("Last problem: %s\n", lastProblem);
+//     }
+// }
