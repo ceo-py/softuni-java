fda3a37df3a97a5690f93b647e7962a91b4c978c
ceo-py <sfmf@abv.bg>
2023-10-21 13:10:40 +0300

    another solution

--- Java Basics/Conditional Statements - Exercise/3_time_15_minutes.java
+++ Java Basics/Conditional Statements - Exercise/3_time_15_minutes.java
@@ -36,3 +36,34 @@ public class Main {
     }
   }
 }
+
+
+//
+// import java.util.Scanner;
+//
+// public class _03_TimePlus15Minutes {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int inputHours = Integer.parseInt(scanner.nextLine());
+//         int inputMinutes = Integer.parseInt(scanner.nextLine());
+//
+//         // Преобразуваме всичко в минути:
+//         int sumMinutes = (inputHours * 60) + inputMinutes + 15;
+//
+//         int hours = sumMinutes / 60;
+//         int minutes = sumMinutes % 60;
+//
+//         if (hours > 23){
+//             hours = 0;
+//         }
+//
+//         if (minutes < 10){
+//             System.out.printf("%d:0%d", hours, minutes);
+//         } else {
+//             System.out.printf("%d:%d", hours, minutes);
+//         }
+//     }
+// }
