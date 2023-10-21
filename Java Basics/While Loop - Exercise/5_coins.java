0519ac705e23236c2a5690bfdd0d47b7aea1603c
ceo-py <sfmf@abv.bg>
2023-10-21 13:21:58 +0300

    another solution

--- Java Basics/While Loop - Exercise/5_coins.java
+++ Java Basics/While Loop - Exercise/5_coins.java
@@ -86,3 +86,61 @@ public class Coins05 {
 //            break;
 //        }
 //        System.out.println(coins);
+
+
+
+
+
+// import java.util.Scanner;
+//
+// public class Coins {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         double change = Double.parseDouble(scanner.nextLine());
+//         double sum = Math.round(change * 100);
+//
+//         int coins = 0;
+//
+//         while (sum > 0) {
+//
+//             if (sum - 200 >= 0) {
+//                 // мога ли да върна 2 лв
+//                 sum -= 200;
+//                 coins++;
+//             } else if (sum - 100 >= 0) {
+//                 // мога ли да върна 1 лв
+//                 sum -= 100;
+//                 coins++;
+//             } else if (sum - 50 >= 0) {
+//                 // мога ли да върна 50 ст
+//                 sum -= 50;
+//                 coins++;
+//             } else if (sum - 20 >= 0) {
+//                 // мога ли да върна 20 ст
+//                 sum -= 20;
+//                 coins++;
+//             } else if (sum - 10 >= 0) {
+//                 // мога ли да върна 10 ст
+//                 sum -= 10;
+//                 coins++;
+//             } else if (sum - 5 >= 0) {
+//                 // мога ли да върна 5 ст
+//                 sum -= 5;
+//                 coins++;
+//             } else if (sum - 2 >= 0) {
+//                 // мога ли да върна 2 ст
+//                 sum -= 2;
+//                 coins++;
+//             } else {
+//                 // мога ли да върна 1 ст
+//                 sum -= 1;
+//                 coins++;
+//             }
+//         }
+//
+//         System.out.println(coins);
+//     }
+// }
