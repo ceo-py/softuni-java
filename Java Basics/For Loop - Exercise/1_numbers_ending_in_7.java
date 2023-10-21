4da6156694f3332daee3317cb26fc13084d55ec2
ceo-py <sfmf@abv.bg>
2023-10-21 13:16:09 +0300

    another solution

--- Java Basics/For Loop - Exercise/1_numbers_ending_in_7.java
+++ Java Basics/For Loop - Exercise/1_numbers_ending_in_7.java
@@ -10,3 +10,30 @@ public class Main {
         }
     }
 }
+
+
+
+// public class _01_NumbersEndingIn7 {
+//
+//     public static void main(String[] args) {
+//
+//         // [1 - 1000]
+//         // Начало: 7
+//         // Край: 997
+//         // Стъпка: +10
+//
+//         // 1ви
+//         for (int number = 7; number <= 997; number += 10) {
+//
+//             System.out.println(number);
+//         }
+//
+//         // 2ри
+// //        for (int number = 7; number <= 997; number++) {
+// //
+// //            if (number % 10 == 7) {
+// //                System.out.println(number);
+// //            }
+// //        }
+//     }
+// }
