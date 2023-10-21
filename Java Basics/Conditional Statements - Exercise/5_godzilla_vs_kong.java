6abac9eb4b788bd13e892514aec91838b8dfb3a0
ceo-py <sfmf@abv.bg>
2023-10-21 13:11:22 +0300

    another solution

--- Java Basics/Conditional Statements - Exercise/5_godzilla_vs_kong.java
+++ Java Basics/Conditional Statements - Exercise/5_godzilla_vs_kong.java
@@ -31,3 +31,40 @@ public class Main {
     }
   }
 }
+
+
+
+//
+// import java.util.Scanner;
+//
+// public class _05_GodzillaVsKong {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         double budget = Double.parseDouble(scanner.nextLine());
+//         int actors = Integer.parseInt(scanner.nextLine());
+//         double pricePerCostume = Double.parseDouble(scanner.nextLine());
+//
+//         // Изчисляваме разходите за филма
+//         double decor = budget * 0.10;
+//         double costumesPrice = actors * pricePerCostume;
+//
+//         // Отстъпка за цената на облеклото при повече от 150 статиста -> 10%
+//         if (actors > 150) {
+//             costumesPrice = costumesPrice - (costumesPrice * 0.10);
+//         }
+//
+//         // Цената за направата на филма
+//         double moviePrice = decor + costumesPrice;
+//
+//         if (moviePrice <= budget) {
+//             System.out.println("Action!");
+//             System.out.printf("Wingard starts filming with %.2f leva left.", budget - moviePrice);
+//         } else {
+//             System.out.println("Not enough money!");
+//             System.out.printf("Wingard needs %.2f leva more.", moviePrice - budget);
+//         }
+//     }
+// }
