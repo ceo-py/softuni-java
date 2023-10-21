bf99db9a8c0a754391b56a3d3ee8baf4b3297c3c
ceo-py <sfmf@abv.bg>
2023-10-21 13:11:01 +0300

    another solution

--- Java Basics/Conditional Statements - Exercise/4_toy_shop.java
+++ Java Basics/Conditional Statements - Exercise/4_toy_shop.java
@@ -37,3 +37,40 @@ public class Main {
     }
   }
 }
+
+
+
+// import java.util.Scanner;
+//
+// public class _04_ToyShop {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         double tripPrice = Double.parseDouble(scanner.nextLine());
+//         int puzzles = Integer.parseInt(scanner.nextLine());
+//         int dolls = Integer.parseInt(scanner.nextLine());
+//         int teddyBears = Integer.parseInt(scanner.nextLine());
+//         int minions = Integer.parseInt(scanner.nextLine());
+//         int trucks = Integer.parseInt(scanner.nextLine());
+//
+//         // Общата цена на поръчката
+//         double orderPrice = (puzzles * 2.60) + (dolls * 3.00) + (teddyBears * 4.10) + (minions * 8.20) + (trucks * 2.00);
+//
+//         // Ако броя на играчките е над/или 50 правим отстъпка от 25%
+//         int toysCount = puzzles + dolls + teddyBears + minions + trucks;
+//         if (toysCount >= 50) {
+//             orderPrice = orderPrice - (orderPrice * 0.25);
+//         }
+//
+//         // Махаме 10% от цената за наем на магазина
+//         orderPrice = orderPrice - (orderPrice * 0.10);
+//
+//         if (orderPrice >= tripPrice) {
+//             System.out.printf("Yes! %.2f lv left.", orderPrice - tripPrice);
+//         } else {
+//             System.out.printf("Not enough money! %.2f lv needed.", tripPrice - orderPrice);
+//         }
+//     }
+// }
