127925c58a97d27cc51aef22ad2becc3f4a299fd
ceo-py <sfmf@abv.bg>
2023-10-21 13:23:33 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/4_game_number_wars.java
+++ Java Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/4_game_number_wars.java
@@ -44,3 +44,51 @@ public class GameNumberWars04 {
         }
     }
 }
+
+
+
+
+// import java.util.Scanner;
+//
+// public class GameNumberWars {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         String firstPlayerName = scanner.nextLine();
+//         String secondPlayerName = scanner.nextLine();
+//
+//         int firstPlayerPoints = 0;
+//         int secondPlayerPoints = 0;
+//
+//         String input = scanner.nextLine();
+//         while (!input.equals("End of game")) {
+//
+//             int firstPlayerCard = Integer.parseInt(input);
+//             int secondPlayerCard = Integer.parseInt(scanner.nextLine());
+//
+//             if (firstPlayerCard > secondPlayerCard) {
+//                 firstPlayerPoints += firstPlayerCard - secondPlayerCard;
+//             } else if (secondPlayerCard > firstPlayerCard) {
+//                 secondPlayerPoints += secondPlayerCard - firstPlayerCard;
+//             } else {
+//                 System.out.println("Number wars!");
+//                 int firstNumberWarCard = Integer.parseInt(scanner.nextLine());
+//                 int secondNumberWarCard = Integer.parseInt(scanner.nextLine());
+//
+//                 if (firstNumberWarCard > secondNumberWarCard) {
+//                     System.out.printf("%s is winner with %d points", firstPlayerName, firstPlayerPoints);
+//                 } else {
+//                     System.out.printf("%s is winner with %d points", secondPlayerName, secondPlayerPoints);
+//                 }
+//                 return;
+//             }
+//
+//             input = scanner.nextLine();
+//         }
+//
+//         System.out.printf("%s has %d points\n", firstPlayerName, firstPlayerPoints);
+//         System.out.printf("%s has %d points\n", secondPlayerName, secondPlayerPoints);
+//     }
+// }
