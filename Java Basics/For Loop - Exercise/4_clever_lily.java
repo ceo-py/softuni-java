9a2f64fdb5a52504bc695b79f3c0f42131f8d0d6
ceo-py <sfmf@abv.bg>
2023-10-21 13:17:14 +0300

    another solution

--- Java Basics/For Loop - Exercise/4_clever_lily.java
+++ Java Basics/For Loop - Exercise/4_clever_lily.java
@@ -35,3 +35,42 @@ public class Main {
         }
     }
 }
+
+
+// import java.util.Scanner;
+//
+// public class CleverLily {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int age = Integer.parseInt(scanner.nextLine());
+//         double washingMachinePrice = Double.parseDouble(scanner.nextLine());
+//         int toyPrice = Integer.parseInt(scanner.nextLine());
+//
+//         int toys = 0;
+//         double lilyMoney = 0.00;
+//         double birthdayMoney = 10.00;
+//
+//         for (int i = 1; i <= age; i++) {
+//             // If the current birthday is even number Lily will earn money.
+//             if (i % 2 == 0) {
+//                 lilyMoney += birthdayMoney;
+//                 birthdayMoney += 10.00;
+//                 lilyMoney -= 1.00;
+//             } else {
+//                 // If the current birthday is odd number Lily will take toy.
+//                 toys++;
+//             }
+//         }
+//
+//         // Adding the money for sold toys
+//         lilyMoney += toys * toyPrice;
+//
+//         if (lilyMoney >= washingMachinePrice) {
+//             System.out.printf("Yes! %.2f", lilyMoney - washingMachinePrice);
+//         }else {
+//             System.out.printf("No! %.2f", washingMachinePrice - lilyMoney);
+//         }
+//
+//     }
+// }
