01da01a6500a1e83169adcc0fcc711c538178d9d
ceo-py <sfmf@abv.bg>
2023-10-21 13:26:04 +0300

    another solution

--- Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/2_easter_party.java
+++ Java Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/2_easter_party.java
@@ -30,3 +30,38 @@ public class EasterParty02 {
 
     }
 }
+
+
+// import java.util.Scanner;
+//
+// public class EasterParty_02 {
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int guests = Integer.parseInt(scanner.nextLine());
+//         double cateringPricePerGuest = Double.parseDouble(scanner.nextLine());
+//         double budget = Double.parseDouble(scanner.nextLine());
+//
+//         double cakePrice = budget * 0.1;
+//         double cateringTotalPrice = guests * cateringPricePerGuest;
+//
+//         if (guests >= 10 && guests <= 15) {
+//             cateringTotalPrice *= 0.85; // cateringTotalPrice = cateringTotalPrice * 0.85;
+//         } else if (guests > 15 && guests <= 20) {
+//             cateringTotalPrice *= 0.80; // cateringTotalPrice = cateringTotalPrice * 0.80;
+//         } else if (guests > 20) {
+//             cateringTotalPrice *= 0.75; //cateringTotalPrice = cateringTotalPrice * 0.75;
+//         }
+//
+//         double totalPrice = cakePrice + cateringTotalPrice;
+//         double diff = Math.abs(budget - totalPrice);
+//
+//         if (totalPrice <= budget) {
+//             System.out.printf("It is party time! %.2f leva left.", diff); // System.out.printf("It is party time! %.2f leva left.", budget - totalPrice);
+//         } else {
+//             System.out.printf("No party! %.2f leva needed.", diff); // System.out.printf("It is party time! %.2f leva left.", totalPrice - budget);
+//         }
+//
+//     }
+// }
