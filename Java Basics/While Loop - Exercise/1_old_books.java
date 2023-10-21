21589e90f685f8faeb56b64a5b8a5508a9d1d258
ceo-py <sfmf@abv.bg>
2023-10-21 13:20:34 +0300

    another solution

--- Java Basics/While Loop - Exercise/1_old_books.java
+++ Java Basics/While Loop - Exercise/1_old_books.java
@@ -24,3 +24,39 @@ public class OldBooks01 {
         }
     }
 }
+
+
+
+// import java.util.Scanner;
+//
+// public class OldBooks {
+//
+//     public static void main(String[] args) {
+//
+//         Scanner scanner = new Scanner(System.in);
+//
+//         String theBookOfAnnie = scanner.nextLine();
+//
+//         int checkedBooks = 0;
+//
+//         // Начало:
+//         String book = scanner.nextLine();
+//
+//         // Условие за край:
+//         while (!book.equals(theBookOfAnnie)) {
+//
+//             if (book.equals("No More Books")) {
+//                 System.out.println("The book you search is not here!");
+//                 System.out.printf("You checked %d books.", checkedBooks);
+//                 return;
+//             }
+//
+//             checkedBooks++;
+//
+//             // Стъпка напред (проверяваме следващата книга.):
+//             book = scanner.nextLine();
+//         }
+//
+//         System.out.printf("You checked %d books and found it.", checkedBooks);
+//     }
+// }
